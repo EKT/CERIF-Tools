47816a23d190d051b384d21c54e31bbf4cce8475
bonisv@ekt.gr <bonisv@ekt.gr@cb9a514a-ca6e-7175-23ab-d45f7167c44a>
2013-08-23 14:38:52 +0000

    Deleting old version.

--- trunk/src/main/java/gr/ekt/cerif/services/second/FederatedIdentifierRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.ekt.cerif.services.second;
-
-import gr.ekt.cerif.entities.second.FederatedIdentifier;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FederatedIdentifierRepository extends CrudRepository<FederatedIdentifier, Long> {
-
-}
