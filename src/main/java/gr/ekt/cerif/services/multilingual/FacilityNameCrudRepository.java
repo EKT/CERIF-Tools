7ab11f26ae93cce83ffa8146affcd4a6b80f4d7b
bonisv@ekt.gr <bonisv@ekt.gr@cb9a514a-ca6e-7175-23ab-d45f7167c44a>
2013-08-23 14:25:36 +0000

    New version CERIF 1.5

--- src/main/java/gr/ekt/cerif/services/multilingual/FacilityNameCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.ekt.cerif.services.multilingual;
-
-import gr.ekt.cerif.features.multilingual.FacilityName;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FacilityNameCrudRepository extends CrudRepository<FacilityName, Long> {
-
-}
