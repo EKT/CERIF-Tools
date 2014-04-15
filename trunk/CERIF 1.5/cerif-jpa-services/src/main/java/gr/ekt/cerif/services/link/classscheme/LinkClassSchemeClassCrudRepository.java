3cde17d1d88b0feb11532f1e6a6ebcec1d9aaad1
bonisv@ekt.gr <bonisv@ekt.gr@cb9a514a-ca6e-7175-23ab-d45f7167c44a>
2014-04-15 10:27:27 +0000

    Repositories for new entities ClassScheme_Class and ClassScheme_OrganisationUnit.

---
+++ trunk/CERIF 1.5/cerif-jpa-services/src/main/java/gr/ekt/cerif/services/link/classscheme/LinkClassSchemeClassCrudRepository.java
@@ -0,0 +1,9 @@
+package gr.ekt.cerif.services.link.classscheme;
+
+import gr.ekt.cerif.entities.link.ClassScheme_Class;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LinkClassSchemeClassCrudRepository extends CrudRepository<ClassScheme_Class, Long> {
+
+}
