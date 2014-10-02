0d103b3ca3009b5372e0b18697ec861d021d06ac
bonisv@ekt.gr <bonisv@ekt.gr@cb9a514a-ca6e-7175-23ab-d45f7167c44a>
2014-10-02 13:52:16 +0000

    Refactoring and extended functionality.

--- CERIF 1.5/cerif-jpa-xml/src/main/java/gr/ekt/cerif/xml/loadingSpecs/LoadingSpecs.java
+++ CERIF 1.5/cerif-jpa-xml/src/main/java/gr/ekt/cerif/xml/loadingSpecs/LoadingSpecs.java
@@ -3,193 +3,55 @@ package gr.ekt.cerif.xml.loadingSpecs;
 import java.util.ArrayList;
 import java.util.List;
 
-import gr.ekt.cerif.enumerations.semantics.ClassEnum;
+import gr.ekt.cerif.CerifEntity;
+import gr.ekt.cerif.xml.loader.CerifToXmlDataLoader;
 import gr.ekt.transformationengine.dataloaders.DataLoadingSpec;
 
 /**
- * @author bonisv
+ * Loading specifications for the {@link CerifToXmlDataLoader}.
  *
+ * @author Nikos Pougounias <pougounias@ekt.gr>
  */
 public class LoadingSpecs extends DataLoadingSpec {
 	
-	private List<String> listOfEntitiesIncluded = new ArrayList<String>();
-	private List<String> listOfEntitiesExcluded = new ArrayList<String>();
-	private List<ClassEnum> entities = new ArrayList<ClassEnum>();
-	private List<ClassEnum> embeddedEntities = new ArrayList<ClassEnum>();
-	private Integer windowSize = 0;
-	private Integer windowOffset = 0;
-	private boolean links = true;
-	private boolean showFedIds = false;
-	private boolean debug;
-	private Object id;
-
+	/**
+	 * The input CERIF entities.
+	 */
+	private List<CerifEntity> entities;
+	
+	/*
+	 * (non-Javadoc)
+	 * @see gr.ekt.transformationengine.dataloaders.DataLoadingSpec#generateNextLoadingSpec()
+	 */
 	@Override
 	public DataLoadingSpec generateNextLoadingSpec() {
 		return this;
 	}
 
 	/**
-	 * @return the entities
+	 * Retrieves the CERIF entities.
+	 * @return a list.
 	 */
-	public List<ClassEnum> getEntities() {
+	public List<CerifEntity> getEntities() {
 		return entities;
 	}
-
+	
 	/**
-	 * @param entities the entities to set
+	 * Sets the CERIF entities.
+	 * @param entities a list.
 	 */
-	public void setEntities(List<ClassEnum> entities) {
+	public void setEntities(List<CerifEntity> entities) {
 		this.entities = entities;
 	}
-
-	/**
-	 * @return the embeddedEntities
-	 */
-	public List<ClassEnum> getEmbeddedEntities() {
-		return embeddedEntities;
-	}
-
-	/**
-	 * @param embeddedEntities the embeddedEntities to set
-	 */
-	public void setEmbeddedEntities(List<ClassEnum> embeddedEntities) {
-		this.embeddedEntities = embeddedEntities;
-	}
-
-	/**
-	 * @return the debug
-	 */
-	public boolean isDebug() {
-		return debug;
-	}
-
-	/**
-	 * @param debug the debug to set
-	 */
-	public void setDebug(boolean debug) {
-		this.debug = debug;
-	}
-
-	/**
-	 * @return the id
-	 */
-	public Object getId() {
-		return id;
-	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(Object id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the listOfEntitiesIncluded
-	 */
-	public List<String> getListOfEntitiesIncluded() {
-		return listOfEntitiesIncluded;
-	}
-
-	/**
-	 * @param listOfEntitiesIncluded the listOfEntitiesIncluded to set
-	 */
-	public void setListOfEntitiesIncluded(List<String> listOfEntitiesIncluded) {
-		this.listOfEntitiesIncluded = listOfEntitiesIncluded;
-	}
-
-	/**
-	 * @return the listOfEntitiesExcluded
-	 */
-	public List<String> getListOfEntitiesExcluded() {
-		return listOfEntitiesExcluded;
-	}
-
-	/**
-	 * @param listOfEntitiesExcluded the listOfEntitiesExcluded to set
-	 */
-	public void setListOfEntitiesExcluded(List<String> listOfEntitiesExcluded) {
-		this.listOfEntitiesExcluded = listOfEntitiesExcluded;
-	}
-
-	/**
-	 * @return the windowOffset
-	 */
-	public Integer getWindowOffset() {
-		return windowOffset;
-	}
-
-	/**
-	 * @param windowOffset the windowOffset to set
-	 */
-	public void setWindowOffset(Integer windowOffset) {
-		this.windowOffset = windowOffset;
-	}
-
-	/**
-	 * @return the windowSize
-	 */
-	public Integer getWindowSize() {
-		return windowSize;
-	}
-
-	/**
-	 * @param windowSize the windowSize to set
-	 */
-	public void setWindowSize(Integer windowSize) {
-		this.windowSize = windowSize;
-	}
-
-	/**
-	 * @return the links
-	 */
-	public boolean isLinks() {
-		return links;
-	}
-
-	/**
-	 * @return the showFedIds
-	 */
-	public boolean isShowFedIds() {
-		return showFedIds;
-	}
-
-	/**
-	 * @param showFedIds the showFedIds to set
-	 */
-	public void setShowFedIds(boolean showFedIds) {
-		this.showFedIds = showFedIds;
-	}
-
-	/**
-	 * @param links the links to set
-	 */
-	public void setLinks(boolean links) {
-		this.links = links;
-	}
 	
-	public boolean isSingleEntity() {
-		return id != null;
-	}
-	
-	public boolean isFindById() {
-		return isSingleEntity() && org.apache.commons.lang.math.NumberUtils.isDigits(id.toString());
-	}
-	
-	public boolean isFindByUUID() {
-		return isSingleEntity() && !isFindById();
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
+	/**
+	 * Adds a CERIF entity to the input.
+	 * @param entity An entity.
 	 */
-	@Override
-	public String toString() {
-		return "LoadingSpecs [listOfEntitiesIncluded=" + listOfEntitiesIncluded
-				+ ", listOfEntitiesExcluded=" + listOfEntitiesExcluded
-				+ ", windowSize=" + windowSize + ", windowOffset=" + windowOffset + ", links="
-				+ links + ", showFedIds=" + showFedIds + "]";
+	public void addEntity(CerifEntity entity) {
+		if (entities == null) {
+			entities = new ArrayList<CerifEntity>();
+		}
 	}
 
-	
 }
--- CERIF 1.5/cerif-jpa-xml/src/main/java/gr/ekt/cerif/xml/output/CerifToXmlOutputGenerator.java
+++ CERIF 1.5/cerif-jpa-xml/src/main/java/gr/ekt/cerif/xml/output/CerifToXmlOutputGenerator.java
@@ -11,14 +11,12 @@ import gr.ekt.cerif.entities.result.ResultPublication;
 import gr.ekt.cerif.entities.second.ElectronicAddress;
 import gr.ekt.cerif.entities.second.FederatedIdentifier;
 import gr.ekt.cerif.entities.second.Funding;
+import gr.ekt.cerif.entities.second.Indicator;
 import gr.ekt.cerif.entities.second.Measurement;
 import gr.ekt.cerif.entities.second.PostalAddress;
 import gr.ekt.cerif.features.semantics.Class;
 import gr.ekt.cerif.features.semantics.ClassScheme;
-import gr.ekt.cerif.services.PersistenceService;
-import gr.ekt.transformationengine.core.OutputGenerator;
-import gr.ekt.transformationengine.core.Record;
-import gr.ekt.transformationengine.core.RecordSet;
+import gr.ekt.cerif.xml.loader.CerifEscapeTool;
 import gr.ekt.cerif.xml.records.base.CerifOrganisationUnitRecord;
 import gr.ekt.cerif.xml.records.base.CerifPersonRecord;
 import gr.ekt.cerif.xml.records.base.CerifProjectRecord;
@@ -30,21 +28,28 @@ import gr.ekt.cerif.xml.records.result.CerifResultPublicationRecord;
 import gr.ekt.cerif.xml.records.second.CerifElectronicAddressRecord;
 import gr.ekt.cerif.xml.records.second.CerifFederatedIdentifierRecord;
 import gr.ekt.cerif.xml.records.second.CerifFundingRecord;
+import gr.ekt.cerif.xml.records.second.CerifIndicatorRecord;
 import gr.ekt.cerif.xml.records.second.CerifMeasurementRecord;
 import gr.ekt.cerif.xml.records.second.CerifPostalAddressRecord;
 import gr.ekt.cerif.xml.records.semantics.CerifClassRecord;
 import gr.ekt.cerif.xml.records.semantics.CerifClassSchemeRecord;
+import gr.ekt.transformationengine.core.OutputGenerator;
+import gr.ekt.transformationengine.core.Record;
+import gr.ekt.transformationengine.core.RecordSet;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.tools.generic.DateTool;
 import org.apache.velocity.tools.generic.EscapeTool;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.velocity.VelocityEngineUtils;
 
 /**
@@ -52,21 +57,49 @@ import org.springframework.ui.velocity.VelocityEngineUtils;
  *
  */
 public class CerifToXmlOutputGenerator extends OutputGenerator {
+	
+	/**
+	 * Logging facility.
+	 */
+	private final Logger logger = Logger.getLogger(CerifToXmlOutputGenerator.class);
 
+	/**
+	 * The velocity engine.
+	 */
 	private VelocityEngine cerifToXmlVelocityEngine;
+	
+	/**
+	 * The template.
+	 */
 	private String template;
+	
+	/**
+	 * The source database.
+	 */
 	private String xmlSourceDatabase;
 	
-	private String resultText;
+	/**
+	 * The encoding.
+	 */
+	private String xmlEncoding = "UTF-8";
+	
+	/**
+	 * Whether to produce the result as a byte array.
+	 */
+	private boolean useByteArray;
+	
+	/**
+	 * The actual result.
+	 */
+	private Object result;
 
-	@Autowired
-	PersistenceService service;
-
+	/**
+	 * Constructor.
+	 */
 	public CerifToXmlOutputGenerator() {
 		super();
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public boolean generateOutput(RecordSet recordSet) {
 
@@ -74,23 +107,24 @@ public class CerifToXmlOutputGenerator extends OutputGenerator {
 			return false;
 		}
 
-		Map map = new HashMap();
+		Map<String, Object> map = new HashMap<String, Object>();
 		
-		ArrayList<Project> projects = new ArrayList<Project>();
-		ArrayList<OrganisationUnit> organisations = new ArrayList<OrganisationUnit>();
-		ArrayList<Person> persons = new ArrayList<Person>();
-		ArrayList<ElectronicAddress> electronicAddresses = new ArrayList<ElectronicAddress>();
-		ArrayList<PostalAddress> postalAddresses = new ArrayList<PostalAddress>();
-		ArrayList<Funding> fundings = new ArrayList<Funding>();
-		ArrayList<Class> classes = new ArrayList<Class>();
-		ArrayList<ClassScheme> classesSchemes = new ArrayList<ClassScheme>();
-		ArrayList<ResultPublication> resultPublications = new ArrayList<ResultPublication>();
-		ArrayList<Facility> facilities = new ArrayList<Facility>();
-		ArrayList<ResultProduct> resultProducts = new ArrayList<ResultProduct>();
-		ArrayList<Service> services = new ArrayList<Service>();
-		ArrayList<Equipment> equipments = new ArrayList<Equipment>();
-		ArrayList<FederatedIdentifier> federatedIdentifiers = new ArrayList<FederatedIdentifier>();
-		ArrayList<Measurement> measurements = new ArrayList<Measurement>();
+		List<Project> projects = new ArrayList<Project>();
+		List<OrganisationUnit> organisations = new ArrayList<OrganisationUnit>();
+		List<Person> persons = new ArrayList<Person>();
+		List<ElectronicAddress> electronicAddresses = new ArrayList<ElectronicAddress>();
+		List<PostalAddress> postalAddresses = new ArrayList<PostalAddress>();
+		List<Funding> fundings = new ArrayList<Funding>();
+		List<Class> classes = new ArrayList<Class>();
+		List<ClassScheme> classesSchemes = new ArrayList<ClassScheme>();
+		List<ResultPublication> resultPublications = new ArrayList<ResultPublication>();
+		List<Facility> facilities = new ArrayList<Facility>();
+		List<ResultProduct> resultProducts = new ArrayList<ResultProduct>();
+		List<Service> services = new ArrayList<Service>();
+		List<Equipment> equipments = new ArrayList<Equipment>();
+		List<FederatedIdentifier> federatedIdentifiers = new ArrayList<FederatedIdentifier>();
+		List<Measurement> measurements = new ArrayList<Measurement>();
+		List<Indicator> indicators = new ArrayList<Indicator>();
 
 		Iterator<Record> it = recordSet.getRecords().iterator();
 		while (it.hasNext()) {
@@ -125,9 +159,13 @@ public class CerifToXmlOutputGenerator extends OutputGenerator {
 		    	federatedIdentifiers.add(((CerifFederatedIdentifierRecord)tmpRecord).getFederatedIdentifier());
 		    } else if (tmpRecord instanceof CerifMeasurementRecord) {
 		    	measurements.add(((CerifMeasurementRecord)tmpRecord).getMeasurement());
+		    } else if (tmpRecord instanceof CerifIndicatorRecord) {
+		    	indicators.add(((CerifIndicatorRecord)tmpRecord).getIndicator());
 		    }
 		}
 
+		CerifEscapeTool tool = new CerifEscapeTool();
+		map.put(tool.getKey(), tool);
 		map.put("esc", new EscapeTool());
 		map.put("date", new DateTool());
 		
@@ -146,15 +184,27 @@ public class CerifToXmlOutputGenerator extends OutputGenerator {
 		map.put("equipments", equipments);
 		map.put("federatedIdentifiers", federatedIdentifiers);
 		map.put("measurements", measurements);
+		map.put("indicators", indicators);
 		
 		map.put("xmlDate", new Date());
 		map.put("xmlSourceDatabase", xmlSourceDatabase);
+		map.put("xmlEncoding", xmlEncoding);
 		
-		@SuppressWarnings("deprecation")
-		String text = VelocityEngineUtils.mergeTemplateIntoString(cerifToXmlVelocityEngine, template, map);
-
-		resultText = text;
-
+		try {
+			if (useByteArray) {
+				ByteArrayOutputStream output = new ByteArrayOutputStream();
+				OutputStreamWriter writer = new OutputStreamWriter(output, xmlEncoding);
+				VelocityEngineUtils.mergeTemplate(cerifToXmlVelocityEngine, template, xmlEncoding, map, writer);
+				writer.flush();
+				writer.close();
+				result = output.toByteArray();
+			} else {
+				result = VelocityEngineUtils.mergeTemplateIntoString(cerifToXmlVelocityEngine, template, xmlEncoding, map);
+			}
+		
+		} catch (Exception e) {
+			logger.error("Could not produce XML from velocity template.", e);
+		}
 		return true;
 	}
 
@@ -181,17 +231,19 @@ public class CerifToXmlOutputGenerator extends OutputGenerator {
 	}
 
 	/**
-	 * @return the resultText
+	 * Returns the result as a byte array.
+	 * @return a byte array.
 	 */
-	public String getResultText() {
-		return resultText;
+	public byte[] getResultAsByteArray() {
+		return (byte[]) result;
 	}
-
+	
 	/**
-	 * @param resultText the resultText to set
+	 * Returns the result as a string.
+	 * @return a string.
 	 */
-	public void setResultText(String resultText) {
-		this.resultText = resultText;
+	public String getResultAsString() {
+		return (String) result;
 	}
 
 	/**
@@ -206,6 +258,26 @@ public class CerifToXmlOutputGenerator extends OutputGenerator {
 	 */
 	public void setXmlSourceDatabase(String xmlSourceDatabase) {
 		this.xmlSourceDatabase = xmlSourceDatabase;
+	}
+
+	/**
+	 * Sets the encoding of the produced XML result.
+	 * @param xmlEncoding The XML encoding.
+	 */
+	public void setXmlEncoding(String xmlEncoding) {
+		this.xmlEncoding = xmlEncoding;
+	}
+	
+	/**
+	 * Returns the encoding of the produced XML result. 
+	 * @return the encoding.
+	 */
+	public String getXmlEncoding() {
+		return xmlEncoding;
+	}
+	
+	public void setUseByteArray(boolean useByteArray) {
+		this.useByteArray = useByteArray;
 	}
 	
 	
