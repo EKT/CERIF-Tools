eb8a316ab47a549a6b4084e7ea35707ddd56bbe2
bonisv@ekt.gr <bonisv@ekt.gr@cb9a514a-ca6e-7175-23ab-d45f7167c44a>
2014-10-31 13:28:26 +0000

    Minor corrections

--- trunk/CERIF 1.5/cerif-jpa-services/src/main/java/gr/ekt/cerif/services/link/person/LinkPersonClassRepository.java
+++ trunk/CERIF 1.5/cerif-jpa-services/src/main/java/gr/ekt/cerif/services/link/person/LinkPersonClassRepository.java
@@ -3,7 +3,6 @@ package gr.ekt.cerif.services.link.person;
 import gr.ekt.cerif.entities.base.Person;
 import gr.ekt.cerif.entities.link.person.Person_Class;
 import gr.ekt.cerif.features.semantics.Class;
-import gr.ekt.cerif.features.semantics.ClassScheme;
 
 import java.util.List;
 
@@ -14,38 +13,16 @@ import java.util.List;
 
 public interface LinkPersonClassRepository {
 
-	Person_Class findById(Long id);
-	
-	Person_Class findByPersonClassSchemeAndClass(Person person, Class theClass, ClassScheme scheme);
-	
-	List<Person_Class> findByPersonClassScheme(Person person, ClassScheme scheme);
-	
 	List <Person_Class> findByPerson(Person person);
+	
+	Person_Class save(Person_Class entity);
+	
+	Iterable<Person_Class> save(Iterable<Person_Class> entityList);
+	
+	void delete(Person_Class entity);
+	
+	void delete(Iterable<Person_Class> entityList);
 	
 	Person_Class findByPersonAndTheClass(Person person, Class theClass);
 	
-	Person_Class findByPersonIdAndClassId(Long personId, Long classId);
-	
-	Person_Class findByPersonIdAndClassUri(Long personId, String classUri);
-	
-	List<Person_Class> findByPersonIdClassSchemeUri(Long personId, String schemeUri);
-	
-	Person_Class findPersClassByPersonIdAndSchemeId(Long personId, Long schemeId);
-
-	Person_Class findPersClassByPersonIdAndSchemeUri(Long personId, String schemeUri);
-	
-	Person_Class findPersonUserState(Long personId);
-	
-	public Person_Class save(Person_Class entity);
-	
-	public Iterable<Person_Class> save(Iterable<Person_Class> entityList);
-	
-	public void delete(Person_Class entity);
-	
-	public void delete(Iterable<Person_Class> entityList);
-	
-	public Person_Class updatePerson_Class(Person_Class personClassToSave, boolean replaceAll, Class classToReplace);
-	
-	public void updatePerson_ClassList(Person person, List<Long> classIds, String classSchemeUri);
-
 }
